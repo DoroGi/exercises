a41d915779298899302e3adbd1329e60cc96f308
Gianmarco Doro <gianmarco.doro@gmail.com>
2020-08-11 22:45:33 +0200

    Add exercise 2.1 follow up question

---
+++ 2-Linked_Lists/2.1-RemoveDups/removeDups2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <unordered_set>
+#include "../DoubleLinkedList.hpp"
+
+using namespace std;
+
+//Solution to follow up question:
+//Without additional data structures O(n^2)
+template <typename T>
+DoubleLinkedList<T>* removeDups(DoubleLinkedList<T>* list)
+{
+    if (list->isEmpty()) return list;
+    node<T>* p1 = list->first();
+    node<T>* p2 = list->first();
+    while (p1 != nullptr)
+    {
+        p2 = p1->next;
+        while(p2 != nullptr)
+        {
+            if (p1->data == p2->data) list->remove(p2);
+            p2 = p2->next;
+        }
+        p1 = p1->next;
+    }
+    return list;
+}
+
+int main()
+{
+    DoubleLinkedList<int>* list = new DoubleLinkedList<int>;
+    list->addFirst(3);
+    list->addFirst(2);
+    list->addFirst(3);
+    list->addFirst(4);
+    list->addFirst(2);
+    list->addFirst(3);
+    list->addFirst(5);
+    list->printContent();
+    DoubleLinkedList<int>* dedupled = removeDups(list);
+    list->printContent();
+
+    delete list;
+    return 0;
+}
