d99c324dfc3a50a3f0dcea57633785a5bb9ac274
Gianmarco Doro <gianmarco.doro@gmail.com>
2020-09-12 17:06:39 +0200

    Small edit to graph

--- data_structures/2-Trees_Tries_Graphs/Graph/Graph.cpp
+++
@@ -1,5 +0,0 @@
-#include <vector>
-#include "Graph.hpp"
-
-template <typename T>
-node<T>::node(T d) : data(d) {};
--- data_structures/2-Trees_Tries_Graphs/Graph/Graph.hpp
+++ data_structures/2-Trees_Tries_Graphs/Graph/Graph.hpp
@@ -6,8 +6,9 @@ class node
 {
 public:
     T data;
+    bool marked;
     std::vector<node*> children;
-    node(T d);
+    node(T d) : data(d), marked(false) {};
 };
 
 template class node<int>;
